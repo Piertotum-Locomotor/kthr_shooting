dc186ab1fca97fc719a8deb486e0c06b592f0464
YuseiIkeda <chys22020@g.nihon-u.ac.jp>
2024-06-08 17:57:39 +0900

    interface vs abstract

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,5 +1,3 @@
-// テストメッセージ
-
 class GameObject {
     static GameObject create(/* */) {
         return new GameObject();
@@ -9,3 +7,31 @@ class GameObject {
         System.out.println("test");
     }
 }
+
+/*
+public class Main {
+    public static void main(String[] args) {
+        GameObject x = new AAA(10);
+        System.err.println(x.a);
+        x.print();
+    }
+}
+
+abstract class GameObject {
+    int a;
+    GameObject(int a0) {
+        a = a0;
+    }
+    abstract void print();
+}
+
+class AAA extends GameObject {
+    AAA(int a0) {
+        super(a0);
+    }
+
+    void print() {
+        System.out.println("test");
+    }
+}
+ */
