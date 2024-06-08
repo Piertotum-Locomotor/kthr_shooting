31f3b0c030f888656ec5f8be9965fedb36b49db1
YuseiIkeda <chys22020@g.nihon-u.ac.jp>
2024-06-08 17:08:15 +0900

    頑張ろう・。・v

--- final/shooting_game/Main.java
+++ final/shooting_game/Main.java
@@ -1,65 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        ComplexNumber x = ComplexNumber.create(2.0, 1.0);
-        ComplexNumber y = ComplexNumber.create(5.0, -3.0);
-        System.out.println(x.abs());
-        System.out.println(y.abs());
-        x.add(y).print();
-        x.mul(y).print();
+        GameObject x = GameObject.create();
+        x.debug();
     }
 }
 
 // テストメッセージ
 
-interface ComplexNumber {
-    static ComplexNumber create(double real0, double imag0) {
-        return new ComplexNumberImpl(real0, imag0);
+interface GameObject {
+    static GameObject create(/* */) {
+        return new GameObjectImpl();
     }
 
-    double getReal();
-
-    double getImag();
-
-    double abs();
-
-    ComplexNumber add(ComplexNumber y);
-
-    ComplexNumber mul(ComplexNumber y);
-
-    void print();
+    void debug();
 }
 
-class ComplexNumberImpl implements ComplexNumber {
-    private double real, imag;
+class GameObjectImpl implements GameObject {
+    // private double ...
 
-    ComplexNumberImpl(double real0, double imag0) {
-        real = real0;
-        imag = imag0;
+    GameObjectImpl(/* */) {
+        // ...
     }
 
-    public double getReal() {
-        return real;
-    }
-
-    public double getImag() {
-        return imag;
-    }
-
-    public double abs() {
-        return Math.sqrt(real * real + imag * imag);
-    }
-
-    public ComplexNumber add(ComplexNumber y) {
-        return new ComplexNumberImpl(real + y.getReal(), imag + y.getImag());
-    }
-
-    public ComplexNumber mul(ComplexNumber y) {
-        double newReal = real * y.getReal() - imag * y.getImag();
-        double newImag = real * y.getImag() + imag * y.getReal();
-        return new ComplexNumberImpl(newReal, newImag);
-    }
-
-    public void print() {
-        System.out.println(real + " + (" + imag + ")i");
+    public void debug() {
+        System.out.println("test");
     }
 }
