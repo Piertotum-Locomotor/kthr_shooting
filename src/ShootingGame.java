ceda78f571afb5567748f62392015a772e2a6aa5
YuseiIkeda <chys22020@g.nihon-u.ac.jp>
2024-06-08 18:05:53 +0900

    abstractで書くやつ

---
+++ src/ShootingGame.java
@@ -0,0 +1,25 @@
+public class ShootingGame {
+    public static void main(String[] args) {
+        GameObject x = new AAA(10);
+        System.err.println(x.a);
+        x.print();
+    }
+}
+
+abstract class GameObject {
+    int a;
+    GameObject(int a0) {
+        a = a0;
+    }
+    abstract void print();
+}
+
+class AAA extends GameObject {
+    AAA(int a0) {
+        super(a0);
+    }
+
+    void print() {
+        System.out.println("test");
+    }
+}
