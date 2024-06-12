9f00a75c61120d57862a649d33d9ff764849666a
YuseiIkeda <chys22020@g.nihon-u.ac.jp>
2024-06-12 19:13:34 +0900

    GPTでプレイヤーマウス操作まで

--- src_2/ShootingGame.java
+++ src_2/ShootingGame.java
@@ -1,56 +1,81 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class ShootingGame {
     public static void main(String[] args) {
-        GameObject x = new Test(10);
-        System.out.println(x.a);
+        JFrame frame = new JFrame("Shooting Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 600); // 仮のサイズ
+        frame.setVisible(true);
+
+        GamePanel panel = new GamePanel();
+        frame.add(panel);
     }
 }
 
-//わーーーーーーーーーーーーーーーーーーー
+class GamePanel extends JPanel implements MouseMotionListener {
+    private Player player;
 
-
-abstract class GameObject {
-    int a;
-    GameObject(int a0) {
-        a = a0;
-    }
-}
-
-class Character extends GameObject {
-    Character(int a0) {
-        super(a0);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Test extends GameObject {
-    Test(int a0) {
-        super(a0);
+    public GamePanel() {
+        player = new Player(10, 100, 100); // 初期位置（仮）
+        addMouseMotionListener(this);
     }
 
-    void print() {
-        System.out.println("test");
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        player.draw(g);
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        player.move(x, y);
+        repaint();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {}
+
+    // 内部クラスとしてGameObjectを定義
+    abstract class GameObject {
+        protected int x, y;
+
+        public GameObject(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    // CharacterをGameObjectから派生
+    abstract class Character extends GameObject {
+        protected int size;
+
+        public Character(int x, int y, int size) {
+            super(x, y);
+            this.size = size;
+        }
+
+        public abstract void draw(Graphics g);
+
+        public void move(int newX, int newY) {
+            x = newX;
+            y = newY;
+        }
+    }
+
+    // PlayerをCharacterから派生
+    class Player extends Character {
+        public Player(int size, int x, int y) {
+            super(x, y, size);
+        }
+
+        @Override
+        public void draw(Graphics g) {
+            g.setColor(Color.RED);
+            g.fillRect(x - size / 2, y - size / 2, size, size);
+        }
     }
 }
