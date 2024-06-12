24c3793da631dcda5e554284411fa444b3bb7340
Piertotum-Locomotor <chuu22007@g.nihon-u.ac.jp>
2024-06-12 18:42:43 +0900

    gameobject

--- main/GameObject.java
+++ main/GameObject.java
@@ -1,5 +1,21 @@
 package main;
 
-public class GameObject {
-    
+abstract class GameObject {
+    Boolean isMovable = true;
+    int coordinateX;
+    int coordinateY;
+    Boolean isAlive;
+    int size;
+
+    abstract Boolean getIsMovable();
+    abstract int getCoordinateX();
+    abstract int getCoordinateY();
+    abstract Boolean getIsAlive();
+    abstract int getSize();
+
+    abstract void setIsMovable();
+    abstract void setCoordinateX();
+    abstract void setCoordinateY();
+    abstract void setIsAlive();
+    abstract void setSize();
 }
