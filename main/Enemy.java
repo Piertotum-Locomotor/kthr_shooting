3d4988f05395cbf81ddc1c1532ebadf19e4ba818
takumi1839 <chtk22009@g.nihon-u.ac.jp>
2024-06-12 19:31:54 +0900

    abstract

--- main/Enemy.java
+++ main/Enemy.java
@@ -3,5 +3,23 @@ package main;
 public class Enemy extends Character {
     int health;
     Double velocity;
+    String getName(){
+        return super.name;
+    }
+    double getBulletOffence(){
+        return super.bulletOffence;
+    }
+    double getBulletVelocity(){
+        return super.bulletVelocity;
+    }
+    void setName(String name){
+        super.name = name;
+    }
+    void setBulletOffence(Double bulletOffence){
+        super.bulletOffence = bulletOffence;
+    }
+    void setBulletVelocity(Double bulletVelocity){
+        super.bulletVelocity = bulletVelocity;
+    }
 }
 
