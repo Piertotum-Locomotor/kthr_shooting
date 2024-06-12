89903b8977a17e415a0359c5c3a8fdff1b712ab8
takumi1839 <chtk22009@g.nihon-u.ac.jp>
2024-06-12 19:22:57 +0900

    abstract

--- main/Player.java
+++ main/Player.java
@@ -2,4 +2,22 @@ package main;
 
 public class Player extends Character{
     int remaining;
+    String getName(){
+        return super.name;
+    }
+    double getBulletOffence(){
+        return super.bulletOffence;
+    }
+    double getBulletVelocity(){
+        return super.bulletVelocity;
+    }
+    void setName(String name){
+        super.name = name;
+    }
+    void setBulletOffence(Double nulletOffence){
+        super.bulletOffence = nulletOffence;
+    }
+    void setBulletVelocity(Double bulletVelocity){
+        super.bulletVelocity = bulletVelocity;
+    }
 }
