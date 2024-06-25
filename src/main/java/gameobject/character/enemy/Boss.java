479cd855ab92b5a0a5c8eb2abd9134b7c370d6ec
YuseiIkeda <chys22020@g.nihon-u.ac.jp>
2024-06-25 13:15:23 +0900

    とりあえず敵が玉を同心円状に打ち出すようにしたよ。（変数の型intが悪さをしているよ。）

--- src/main/java/gameobject/character/enemy/Boss.java
+++ src/main/java/gameobject/character/enemy/Boss.java
@@ -8,14 +8,33 @@ import java.awt.*;
 import GameRegistrer.GameRegistrer;
 
 public class Boss extends Enemy {
+    private double currentAngle;
 
     public Boss(int x, int y, int size, Color color, String name, double bulletOffence, int bulletVelocityX, int bulletVelocityY, int shootInterval, int health) {
         super(x, y, size, color, name, bulletOffence, bulletVelocityX, bulletVelocityY, shootInterval, health);
+        this.currentAngle = 0; // 初期角度を0に設定
     }
 
     public void shoot() {
-        Bullet bullet = new Bullet(super.getCoordinateX(), super.getCoordinateY(), super.getSize()/2, super.getBulletVelocityX(), super.getBulletVelocityY(), super.getColor(), Boss.class, Player.class, super.getBulletOffence());
-        GameRegistrer.gameRegisterer(bullet);
+        int bulletCount = 12; // 同心円状に発射する弾の数
+        double angleStep = 360.0 / bulletCount;
+        int bulletSpeed = 6; // 弾の基本速度
+
+        for (int i = 0; i < bulletCount; i++) {
+            double angle = Math.toRadians(currentAngle + i * angleStep);
+            int bulletVelocityX = (int)(bulletSpeed * Math.cos(angle));
+            int bulletVelocityY = (int)(bulletSpeed * Math.sin(angle));
+            
+            Bullet bullet = new Bullet(super.getCoordinateX(), super.getCoordinateY(), super.getSize() / 2, bulletVelocityX, bulletVelocityY, super.getColor(), Boss.class, Player.class, super.getBulletOffence());
+            GameRegistrer.gameRegisterer(bullet);
+        }
+
+        // 打ち出す角度を変えていくやつ
+        // だが、変数の型(int)が悪さをしているため、思うような軌道にはならない
+        currentAngle += 0;
+        if (currentAngle % 360 == 0) {
+            currentAngle = 0;
+        }
     }
 
     public void draw(Graphics g) {
